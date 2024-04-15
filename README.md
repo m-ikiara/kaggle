2621c2747b78d76d4614cf64fddc403d1f24cb57
m-ikiara <83048757+m-ikiara@users.noreply.github.com>
2024-04-15 11:56:33 -0400

    Ready. Set. Go!

---
+++ README.md
@@ -0,0 +1,23 @@
+<div align="center">
+    <h1>kaggle</h1>
+    <img alt="GitHub repo size" src="https://img.shields.io/github/repo-size/m-ikiara/kaggle?style=plastic&color=red" />
+</div>
+
+## Getting Started
+
+1. Clone the repo:
+
+```console
+$ git clone https://github.com/m-ikiara/kaggle.git
+```
+
+2. Change the code and optimize it.
+
+## References
+
+- (2012) Titanic - Machine Learning from Disaster by Will Cukierski @wcuk: [Kaggle](https://kaggle.com/competitions/titanic)
+- (2016) House Prices - Advanced Regression Techniques by Anna Montoya, DataCanary: [Kaggle](https://kaggle.com/competitions/house-prices-advanced-regression-techniques)
+
+## Licensing
+
+This project is licensed under the [MIT License](./LICENSE) at your option.
---
+++ titanic/titanic.py
@@ -0,0 +1,55 @@
+"""Titanic - Machine Learning from Disaster.
+
+My first project in Data Science. Yay!
+"""
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier as rfc
+import os
+
+data = {
+    'train_data': pd.read_csv("/kaggle/input/titanic/train.csv"),
+    'test_data': pd.read_csv("/kaggle/input/titanic/test.csv"),
+    'gender_data': pd.read_csv("/kaggle/input/titanic/gender_submission.csv")
+}
+
+def display_tree():
+    """Output the current working path."""
+    for dirname, _, filenames in os.walk('/kaggle/input'):
+        for filename in filenames:
+            print(os.path.join(dirname, filename))
+
+
+def display_data():
+    """Output the data contained in file."""
+    for k in data:
+        data[k].head()
+
+
+def display_stats_by_gender(gender, status):
+    """Output the statistics for the women in the sinking ship."""
+    if gender == 'male' or gender == 'female':
+        survivors = data["train_data"].loc[data["train_data"].Sex == gender][status]
+        survival_rate = sum(survivors) / len(survivors)
+        print(f"[INFO] % of {gender}s that {status}: {survival_rate * 100}")
+
+def generate_forest():
+    """Create a random forest model."""
+    train_y = data["train_data"]['Survived']
+    features = ['Pclass', 'Sex', "SibSp", "Parch"]
+    train_x = pd.get_dummies(data['train_data'][features])
+    test_x = pd.get_dummies(data['test_data'][features])
+    model = rfc(n_estimators=100, max_depth=5, random_state=1)
+    model.fit(train_x, train_y)
+    predictions = model.predict(test_x)
+    
+    output = pd.DataFrame({
+        'PassengerId': data['test_data'].PassengerId,
+        'Survived': predictions
+    })
+    output.to_csv('submission.csv', index=False)
+    print("[INFO] Your submission was successfully saved!")
+    
+generate_forest()
+submission = pd.read_csv("/kaggle/working/submission.csv")
+submission.head()
